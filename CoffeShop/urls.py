111e2e422e13fee049db8ab71e3531d205f7d44f
Ілля <illjahomenko15@gmail.com>
2022-08-09 17:28:00 +0300

    Test

--- CoffeShop/urls.py
+++ CoffeShop/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.landing),
-    
+    path('', views.landing)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- coffe/views.py
+++ coffe/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+
 # Create your views here.
 def landing(request):
     return render (request, 'coffe/landing.html')
